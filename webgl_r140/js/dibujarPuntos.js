5fdc12263c816bc03c1dc6d87c1284408c1514a8
andreu <andreucs@kth.se>
2022-09-20 19:07:26 +0200

    añadir dibujar linia i cambios de color

--- webgl_r140/js/dibujarPuntos.js
+++ webgl_r140/js/dibujarPuntos.js
@@ -5,9 +5,13 @@
 //Shader de vertices
 const VSHADER_SOURCE = `
     attribute vec3 posicion;
+    varying highp vec4 vColor
     void main(){
         gl_Position = vec4(posicion,1.0);
         gl_PointSize = 10.0;
+        // EL s.r es de 2x2 y esta centrat:
+        highp float distancia = sqrt(dot(posicion.xy, posicion.xy))
+        vColor = vec4(1.0 - distancia, 1.0 - distancia, 0.0, 1.0)
     }
 `
 
@@ -38,7 +42,7 @@ function main(){
     gl.clearColor(0.0, 0.0 ,0.3 ,1.0);
 
     //Localiza atributo shaders posicion
-    const coordenadas = gl.getAttribLocation(gl.program, 'posicion'); //devuelvo el attributo posicion 
+    var coordenadas = gl.getAttribLocation(gl.program, 'posicion'); //devuelvo el attributo posicion 
 
     //Crear Buffer, etc..
     const bufferVertices = gl.createBuffer();
@@ -46,11 +50,11 @@ function main(){
     gl.vertexAttribPointer( coordenadas, 3, gl.FLOAT, false, 0, 0 ); //enllaça bufferVertices amb coordenadas
     gl.enableVertexAttribArray( coordenadas ); //activa la matriu d'atributs de vèrtex genèric a l'índex especificat a la llista de matrius d'atributs
 
-    // Asignar el mismo color a todos los puntos
-    colorFragmento = gl.getUniformLocation( gl.program, 'color' );
+    // Asignar el mismo color a todos los puntos (ja no es constante el color)
+    // colorFragmento = gl.getUniformLocation( gl.program, 'color' );
 
     // Registrar la call-back del click del raton
-    canvas.onmousedown = function(evento){ click(evento,gl,canvas); };
+    canvas.onclick = function(evento){ click(evento,gl,canvas); };
 
     // Dibujar
     render( gl );
@@ -63,9 +67,9 @@ function click( evento, gl, canvas )
     // del documento. Los puntos que se pasan al shader deben
     // de estar en el cuadrado de lado dos centrado en el canvas
 
-    let x = evento.clientX;
-    let y = evento.clientY;
-    const rect = evento.target.getBoundingClientRect();
+    var x = evento.clientX;
+    var y = evento.clientY;
+    var rect = evento.target.getBoundingClientRect();
 
     // Conversion de coordenadas al sistema webgl por defecto
     x = ((x-rect.left)-canvas.width/2) * 2/canvas.width;
@@ -81,13 +85,13 @@ function click( evento, gl, canvas )
 
 function render( gl )
 {
+    var puntos = new Float32Array(clicks) //array de todos los puntos guardados
 	// Borra el canvas con el color de fondo
 	gl.clear( gl.COLOR_BUFFER_BIT );
 
-	// Fija el color de TODOS los puntos
-	gl.uniform3f(colorFragmento, 1, 1, 0);
-
 	// Rellena el BO activo con las coordenadas y lo manda a proceso
-	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(clicks), gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferVertices)
+	gl.bufferData( gl.ARRAY_BUFFER, puntos, gl.STATIC_DRAW );
+    gl.drawArrays( gl.POINTS, 0, clicks.length/3 )	
 	gl.drawArrays( gl.LINE_STRIP, 0, clicks.length/3 )	
 }
