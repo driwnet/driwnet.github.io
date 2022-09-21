152e450fd1269edf2cfa305e5e23d19eb1cd9afc
andreu <andreucs@kth.se>
2022-09-21 17:10:36 +0200

    corregir error

--- webgl_r140/js/Robot.js
+++ webgl_r140/js/Robot.js
@@ -6,6 +6,9 @@
  * Author: Andreu Casamayor
  */
 
+//Imports
+import * as THREE from "../lib/three.module.js";
+
 //Variables De consenso
 let renderer, scene, camera;
 
@@ -30,7 +33,21 @@ function init()
     scene.background = new THREE.Color(0.5,0.5,0.5);
 
     //Instanciar camera
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 100);
-    camera.position.set(0.5,2,7);
-    camera.lookAt(0,1,0);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
+    camera.position.set(90, 200, 350);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+}
+
+function loadScene()
+{
+    //Material
+    const material = new THREE.MeshBasicMaterial({color: "red", wireframe: true});
+
+    //Robot completo
+    robot = new THREE.Object3D();
+
+    //Base
+    base = new THREE.Mesh(THREE.CylinderGeometry(50, 50, 15, 32), material);
+
+    
 }
