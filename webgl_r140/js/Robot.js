86dbbfe60afa5b2d508f702559e7802e69aa6557
andreu <andreucs@kth.se>
2022-09-22 11:51:11 +0200

    acabar classe Robot.js

--- webgl_r140/js/Robot.js
+++ webgl_r140/js/Robot.js
@@ -77,19 +77,19 @@ function loadScene()
 
     //elemento nervio 1:
     var nervio1 = new THREE.Mesh(THREE.BoxGeometry(4,80,4), material);
-    nervio1.position.set(4, 0, -4);
+    nervio1.position.set(8, 0, -4);
     antebrazo.add(nervio1);
     //elemento nervio 2:
     var nervio2 = new THREE.Mesh(THREE.BoxGeometry(4,80,4), material);
-    nervio2.position.set(-4, 0, -4);
+    nervio2.position.set(-8, 0, -4);
     antebrazo.add(nervio2);
     //elemento nervio 3:
     var nervio3 = new THREE.Mesh(THREE.BoxGeometry(4,80,4), material);
-    nervio3.position.set(4, 0, 4);
+    nervio3.position.set(8, 0, 4);
     antebrazo.add(nervio3);
     //elemento nervio 4:
     var nervio4 = new THREE.Mesh(THREE.BoxGeometry(4,80,4), material);
-    nervio4.position.set(-4, 0, 4);
+    nervio4.position.set(-8, 0, 4);
     antebrazo.add(nervio4);
 
     //Elemento mano (3 elementos:mano, pinzalz, pinzaDe)
@@ -97,5 +97,77 @@ function loadScene()
     mano.position.set(0,80,0);
     mano.rotateZ(Math.PI/2);
 
+    //Pinzas
+    var pinzageo = new THREE.BufferGeometry();
+    const vertices = [
+        0, -8, -10, //0
+        19, -8, -10, //1
+        0, -8, 10, //2 (20 altura --> -10 --> 10)
+        19, -8, 10, //3
+        0, -12, -10, //4
+        0,-12, 10, //5
+        19, -12, 10,//6
+        19, -12, -10, //7
+        38, -8, -5, //8
+        38, -8, 5, //9
+        38, -10, -5, //10
+        38, -10, 5 //11
+    ];
+
+    const indices = [
+        0,1,3, //cara 0
+        0,2,3, //cara 1
+        0,4,5, //cara 2
+        0,2,5, //cara 3
+        0,1,7, //cara 4
+        0,4,7, //cara 5
+        4,5,6, //cara 6
+        4,3,6, //cara 7
+        2,3,6, //cara 8
+        2,5,6, //cara 9
+        1,8,9, //cara 10
+        1,3,9, //cara 11
+        1,10,8, //cara 12
+        1,7,10, //cara 13
+        7,10,11, //cara 14
+        7,6,11, //cara 15
+        3,6,11, //cara 16
+        3,9,11, //cara 17
+        8,10,9, //cara 18
+        10,9,11, //cara 19
+        1,7,3, //cara 20
+        7,6,3 //cara 21
+    ];
+
+    pinzageo.setIndex( indices );
+    pinzageo.setAttribute( 'position', new THREE.Float32BufferAttribute(vertices,3));
     
+    //pinza izquierda
+    var pinzaI = new THREE.Mesh(pinzageo, material);
+    mano.add(pinzaI);
+    //pinza derecha
+    var pinzaD = new THREE.Mesh(geopinza, matRobot);
+    pinzaD.rotateY(Math.PI / 2)
+    pinzaD.position.set(0, 20, 0)
+    mano.add(pinzaD);
+
+    antebrazo.add(mano);
+    brazo.add(antebrazo);
+    base.add(brazo);
+    robot.add(base);
+    scene.add(robot);
+
+    //Suelo
+    const materialSuelo = new THREE.MeshBasicMaterial({color: "yellow", wireframe: true});
+    const suelo = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 10,10), materialSuelo);
+    suelo.rotateX(-Math.PI/2);
+    scene.add(suelo);
+}
+
+//funcion renderer()
+function render()
+{
+    requestAnimationFrame(render);
+    update();
+    renderer.render(scene, camera);
 }
